36b5e0dc35b703a1656eb18e153e69cec44cf5e1
Ethan Bundy <gunderodd@hotmail.com>
2020-02-10 07:14:35 -0800

    added images, webp images, landscape illustration, and js to adjust character size

--- hide.js
+++ hide.js
@@ -1,13 +1,47 @@
-const buffalo = document.getElementById("buffalo_1")
-const grid = document.getElementsByClassName("portfolio_grid")
+// change the buffalo man's position so that he doesn't obscure too much of the portfolio
 
-if (grid.style.top >= 0 && grid.style.bottom < screen.height) {
-    console.log(yes)
-} else {
-    console.log(no)
-}
+// select the portfolio grid and the buffalo man
+const myGrid = document.querySelector('.portfolio_grid')
+const buffalos = document.querySelectorAll('.buffalo_man')
 
-console.log(grid)
-console.log(buffalo)
+observer = new IntersectionObserver((entries) => {
 
-// next, try to find a way to detect if the portfolio_grid is on screen
+    // if the grid is visible on the viewport, lower the buffalo man and zoom on his face
+    console.log(entries)
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            buffalos.forEach(buffalo => {
+                buffalo.classList.add('hide')
+            })
+        } else {
+            buffalos.forEach(buffalo => {
+                buffalo.classList.remove('hide')
+            })
+        }
+    })
+})
+
+observer.observe(myGrid)
+
+// make the buffalo man smaller so that he appears to fall into the well
+
+const landscape = document.querySelector('#landscape')
+// const buffalos = document.querySelectorAll('.buffalo_man')
+
+observer = new IntersectionObserver((entries) => {
+
+    console.log(entries)
+    entries.forEach(entry => {
+        if (entry.intersectionRatio > 0) {
+            buffalos.forEach(buffalo => {
+                buffalo.classList.add('fall')
+            })
+        } else {
+            buffalos.forEach(buffalo => {
+                buffalo.classList.remove('fall')
+            })
+        }
+    })
+})
+
+observer.observe(landscape)
